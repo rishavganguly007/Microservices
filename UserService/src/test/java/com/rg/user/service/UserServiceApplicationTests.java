8133bdde732f1e6cbc74c16d5adc497a3e600dd8
rishavganguly007 <rishavganguly007@gmail.com>
2023-03-30 23:30:44 +0530

    Added Service Registry and netflix-eureka-client for service communications

--- UserService/src/test/java/com/rg/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/rg/user/service/UserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.rg.user.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
+@EnableEurekaClient
 class UserServiceApplicationTests {
 
 	@Test
